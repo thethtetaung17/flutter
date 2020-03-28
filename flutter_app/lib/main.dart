166c5b02dd92879b43bb3228d70ed236e8ab7435
Thet Htet Aung <thethtetaung17@gmail.com>
2020-03-28 13:44:01 +0630

    Stateless Widget and Hot Reload

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("First Flutter App"),
-          backgroundColor: Colors.red[600],
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            "Hello Flutter!",
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, letterSpacing: 2.0, color: Colors.grey[600], fontFamily: 'Playfair'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text("+"),
-          onPressed: () {},
-          backgroundColor: Colors.red[600],
-        ),
-      ),
+      home: Home()
     ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First Flutter App"),
+        backgroundColor: Colors.red[600],
+        centerTitle: true,
+      ),
+      body: Center(
+        child:
+//        Image(
+//          image: NetworkImage('https://cdn.wallpapersafari.com/96/53/QXwfgq.jpg'),
+//        )
+        Text(
+          "Hello Flutter!",
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, letterSpacing: 2.0, color: Colors.grey[600], fontFamily: 'Playfair'),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text("+"),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
